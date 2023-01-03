0182a3ce8c0abfe6d046ca1cd7d8a17942d8367f
Yiğit Küçük <yigitkucukbusiness@gmail.com>
2023-01-03 20:47:03 +0300

    Added back arrow to settings

--- app/src/main/java/com/example/lalbrowser/SettingsActivity.java
+++ app/src/main/java/com/example/lalbrowser/SettingsActivity.java
@@ -22,6 +22,8 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
